f67943fbb6b80a7fa5745eab1cb07548606358d9
agonzg40 <47633256+agonzg40@users.noreply.github.com>
2022-05-02 13:30:23 +0200

    All done

--- prueba-camara/cv_basics/cv_basics/webcam_sub.py
+++ prueba-camara/cv_basics/cv_basics/webcam_sub.py
@@ -10,8 +10,6 @@ from torch import hub # Hub contains other models like FasterRCNN
 
 thres = 0.45 # Threshold to detect object
 
-#cap = cv2.VideoCapture(0)
-
 classFile = 'src/prueba-camara/cv_basics/cv_basics/coco.names'
 with open(classFile,'rt') as f:
   classNames = f.read().rstrip('\n').split('\n')
@@ -25,78 +23,32 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-#while True:
-      #success,img = cap.read()
-      #classIds, confs, bbox = net.detect(img,confThreshold=thres)
-      #print(classIds,bbox)
- 
-      #if len(classIds) != 0:
-        #for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-            #cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-            #cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-            #cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            #cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-            #cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-     
-    # Convert to grayscale
-    #gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-    
-    # Detect the faces
-    #faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    
-    # Draw the rectangle around each face
-    #for (x, y, w, h) in faces:
-        #cv2.rectangle(current_frame, (x, y), (x+w, y+h), ((0, 255, 0)), 2)
-        #cv2.addText(img=current_frame, text="Face", org=(x ,y), nameFont="Times", pointSize=15, color=(255, 0, 0))
-    
-    # Display image
-      #cv2.imshow("camera", img)
-    
-      #cv2.waitKey(1)
 
-#face_cascade = cv2.CascadeClassifier('src/prueba-camara/cv_basics/cv_basics/haarcascade_frontalface_default.xml')
-
- 
 class ImageSubscriber(Node):
-  """
-  Create an ImageSubscriber class, which is a subclass of the Node class.
-  """
   def __init__(self):
-    """
-    Class constructor to set up the node
-    """
-    # Initiate the Node class's constructor and give it a name
+
     super().__init__('image_subscriber')
       
-    # Create the subscriber. This subscriber will receive an Image
-    # from the video_frames topic. The queue size is 10 messages.
+    #Subscribe the token
     self.subscription = self.create_subscription(
       Image, 
       'video_frames', 
       self.listener_callback, 
       10)
-    self.subscription # prevent unused variable warning
+    self.subscription
       
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
    
   def listener_callback(self, data):
-    """
-    Callback function.
-    """
-    # Display the message on the console
-    #self.get_logger().info('Receiving video frame')
  
     # Convert ROS Image message to OpenCV image
     current_frame = self.br.imgmsg_to_cv2(data)
 
-    
-
-    #while True:
-      #success,img = data.read()
+    # Detect objects
     classIds, confs, bbox = net.detect(current_frame,confThreshold=thres)
-    print("eh", classIds,bbox)
  
+    #Put the rectangle around the object and the text
     if len(classIds) != 0:
       for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
           cv2.rectangle(current_frame,box,color=(0,255,0),thickness=2)
@@ -105,28 +57,19 @@ class ImageSubscriber(Node):
           cv2.putText(current_frame,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
           cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
     
-    # Display image
       cv2.imshow("camera", current_frame)
     
       cv2.waitKey(1)
   
 def main(args=None):
-  
-  # Initialize the rclpy library
+
   rclpy.init(args=args)
-  
-  # Create the node
   image_subscriber = ImageSubscriber()
   
-  # Spin the node so the callback function is called.
   rclpy.spin(image_subscriber)
-  
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
+
   image_subscriber.destroy_node()
   
-  # Shutdown the ROS client library for Python
   rclpy.shutdown()
   
 if __name__ == '__main__':
